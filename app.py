6c81d28b2d5cfaeaf2302932ced2a1152a11bf90
Sandratra <nysandratrandrianahary@gmail.com>
2023-07-06 13:45:53 +0300

    first login back commit

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_wtf import FlaskForm
 from flask_bcrypt import Bcrypt
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
+from config import SECRET_KEY
 # from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -16,60 +17,18 @@ CORS(app)
 
 
 
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SECRET_KEY'] = '794b05a8a28df217be8ad971574efd1c4cbfe430c76048ab2e5825c81a958a8e'
+
 bcrypt = Bcrypt(app)
 
 # Configuration de la connexion MySQL
-db = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="",
-  database="etsabodb"
+connection = mysql.connector.connect(
+  host='localhost',
+  user='root',
+  password='',
+  database='etsabodb'
 )
-cursor = db.cursor()
-
-
-
-# @app.route("/<name>")
-# def hello(name):
-#     return f"Hello, {escape(name)}!"
-
-
-    # class User(db.Model):
-    # idPerson = db.Column(db.Integer, primary_key=True)
-    # namePerson = db.Column(db.String(255), unique=False, nullable=False)
-    # username = db.Column(db.String(255), unique=True, nullable=False)
-    # emailAddress = db.Column(db.String(255), unique=True, nullable=False)
-    # password = db.Column(db.String(255), nullable=False)
-    # birthdate = db.Column(db.Date, nullable=False)
-    # gender = db.Column(db.Boolean, nullable=False) #M: 1 ; F:0
-        
-
-    # def __repr__(self):
-    #     return f"User('{self.username}', '{self.emailAddress}')"
-
-
-# class RegistrationForm(FlaskForm):
-#     # ...
-
-#     def validate_username(self, username):
-#         # Vérifier si le nom d'utilisateur est déjà utilisé
-#         user = User.query.filter_by(username=username.data).first()
-#         if user:
-#             raise ValidationError('Ce nom d\'utilisateur est déjà utilisé. Veuillez en choisir un autre.')
-
-#     def validate_email(self, emailAddress):
-#         # Vérifier si l'e-mail est déjà utilisé
-#         user = User.query.filter_by(emailAddress=emailAddress.data).first()
-#         if user:
-#             raise ValidationError('Cet e-mail est déjà utilisé. Veuillez en choisir un autre.')
-
-#     def save_user(self):
-#         # Enregistrer l'utilisateur dans la base de données
-#         hashed_password = bcrypt.generate_password_hash(self.password.data).decode('utf-8')
-#         user = User(username=self.username.data, emailAddress=self.email.data, password=hashed_password)
-#         db.session.add(user)
-#         db.session.commit()
+cursor = connection.cursor()
 
 #######################################################
 class RegistrationForm(FlaskForm):
@@ -94,12 +53,12 @@ class RegistrationForm(FlaskForm):
         query = "INSERT INTO users (username, email, password) VALUES (%s, %s, %s)"
         hashed_password = bcrypt.generate_password_hash(self.password.data).decode('utf-8')
         cursor.execute(query, (self.username.data, self.email.data, hashed_password))
-        db.commit()
+        connection.commit()
 
 ##########################      
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
+@app.route('/api/registration', methods=['GET', 'POST'])
+def registration():
     registration_form = RegistrationForm()
     # login_form = LoginForm()
 
@@ -107,30 +66,54 @@ def home():
         registration_form.save_user()
         flash('Account created successfully!', 'success')
         return redirect(url_for('home'))
-    return "eto isika izao"
+   
     # ...
 
+@app.route('/')
+def home():
+    return ''
+
 #######################################################
-@app.route('/login', methods=['POST'])
+@app.route('/api/login', methods=['POST'])
 def login():
     # Récupérer les données d'identification de l'utilisateur depuis la requête POST
     username = request.json['username']
     password = request.json['password']
 
     # Vérifier les informations d'identification dans la base de données
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM users WHERE username = %s", (username,))
+    cur = connection.cursor()
+    cur.execute("SELECT username,password FROM users WHERE username = %s", (username,))
     user = cur.fetchone()
     cur.close()
+    connection.close()
 
     if user:
         # Vérifier le mot de passe
-        if password == user['password']:
+        if password == user[1]:
+            # if password == user['password']:
             # Authentification réussie
-            return jsonify({'message': 'Login successful'})
+            response = {
+                'success': True,
+                'message': 'Authentification réussie',
+                'token': 'abc123'  # Exemple de jeton d'accès généré
+            }
+            
         else:
             # Mot de passe incorrect
-            return jsonify({'message': 'Invalid password'})
+            response = {
+                'success': False,
+                'message': 'Votre mot de passe est incorrect',
+                
+            }
+        return jsonify(response)
     else:
         # Utilisateur non trouvé
-        return jsonify({'message': 'User not found'})
+        response = {
+                'success': False,
+                'message': 'Utilisateur non trouvé',
+                
+            }
+        return jsonify(response)
+
+if __name__ == '__main__':
+    app.run()
