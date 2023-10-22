a87a5bac914783f06c58ba153c87736b70f3f803
Sandratra <nysandratrandrianahary@gmail.com>
2023-10-22 11:55:59 +0300

    ajout get list doc

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ from flask_cors import CORS
 from flask_wtf import FlaskForm
 from flask_bcrypt import Bcrypt
 from routes.user_route import user_route
+from routes.doctor_route import doctor_route
 from decouple import Config,Csv
 from flask_jwt_extended import jwt_required, get_jwt_identity, JWTManager
-import logging                  
+import logging      
+from database import create_new_connection            
 
 from config import config
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "*"}})
+CORS(app)
 app.config['SECRET_KEY'] = 'fyfedsqdohazjepoka785'
 app.config['DEBUG'] = True
 app.config['JWT_TOKEN_LOCATION'] = ['headers']
@@ -49,8 +51,35 @@ def send():
             response = {"error": error_message}
             return jsonify(response), 500
 
+# @app.route('/api/doctor_route/listdocs', methods=['GET'])
+# def getListDocs():
+#     try:
+#         connection = create_new_connection()
+#         cur = connection.cursor()
+#         cur.execute("SELECT id,name,spectialite FROM users WHERE isDoc = 1")
+#         doctors = cur.fetchall();
+#         cur.close()
+#         connection.close()
+
+#         if doctors:
+#             response = {
+#                 'success' : True,
+#                 'doctors' : doctors
+#             }
+#         else:
+#             response = {
+#                 'success' : False,
+#                 'message' : "Il n'y a pas de docteur disponible "
+#             }
+#             return jsonify(response)
+        
+#     except Exception as e:
+#         error_message = str(e)
+#         app.logger.error(f'Erreur lors de la récupération des docteurs : {e}')
+#         return jsonify({'error': error_message}), 500
             
 app.register_blueprint(user_route)
+app.register_blueprint(doctor_route)
 
 if __name__ == '__main__':
     app.run()
